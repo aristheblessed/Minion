5956a158f96c38406a51b88ec9681a545340e9ec
aristheblessed <32597789+aristheblessed@users.noreply.github.com>
2017-10-08 12:45:42 +0200

    Update database.php
    update database

--- config/database.php
+++ config/database.php
@@ -1,9 +1,14 @@
-<?php
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $db = new PDO ('mysql:host=localhost; dbname=minions', 'root', '' );
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $db = new PDO("sqlsrv:server = tcp:laptop-g4pdd4eo.database.windows.net,1433; Database = Minions", "minionAdmin", "Miniononthego123");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-}
-catch (PDOException $e ) {
-    die('Error:' .$e->getMessage());
-}
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "minionAdmin@laptop-g4pdd4eo", "pwd" => "Miniononthego123", "Database" => "Minions", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:laptop-g4pdd4eo.database.windows.net,1433";
+$db = sqlsrv_connect($serverName, $connectionInfo);
